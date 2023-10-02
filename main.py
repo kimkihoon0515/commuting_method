e0ff3a42f3e4e81fc04d7840cdcce99095862419
kimkihoon0515 <tega1996@naver.com>
2023-10-03 00:28:15 +0900

    selenium webdriver fixed

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 import time
 
+service = Service()
 options = webdriver.ChromeOptions()
 options.add_argument("headless")
 
-driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome(service=service, options=options)
 
 url = "https://map.naver.com/p/directions/14129734.6092034,4490014.3475417,%EA%B2%BD%EA%B8%B0%20%EA%B5%B0%ED%8F%AC%EC%8B%9C%20%EA%B3%A0%EC%82%B0%EB%A1%9C%20596-15%20%EC%82%B0%EB%B3%B8%20%EC%9D%B4%ED%8E%B8%ED%95%9C%EC%84%B8%EC%83%81%20%EC%84%BC%ED%8A%B8%EB%9F%B4%ED%8C%8C%ED%81%AC,,ADDRESS_POI/14138490.1988488,4507890.0897346,%EC%84%9C%EC%B4%88%EC%97%AD1%EB%B2%88%EC%B6%9C%EA%B5%AC,21406506,PLACE_POI/-/car?c=11.00,0,0,0,dh"
 # url = "https://map.naver.com/p/directions/14129734.6092034,4490014.3475417,%EA%B2%BD%EA%B8%B0%20%EA%B5%B0%ED%8F%AC%EC%8B%9C%20%EA%B3%A0%EC%82%B0%EB%A1%9C%20596-15%20%EC%82%B0%EB%B3%B8%20%EC%9D%B4%ED%8E%B8%ED%95%9C%EC%84%B8%EC%83%81%20%EC%84%BC%ED%8A%B8%EB%9F%B4%ED%8C%8C%ED%81%AC,,ADDRESS_POI/14138490.1988488,4507890.0897346,%EC%84%9C%EC%B4%88%EC%97%AD1%EB%B2%88%EC%B6%9C%EA%B5%AC,21406506,PLACE_POI/-/walk?c=11.00,0,0,0,dh"
